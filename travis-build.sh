cd89516304550b72d6c2a36c3d9154df84711312
Uri Herrera <hello@uriherrera.com>
2020-12-09 22:21:51 -0600

    create branch

--- travis-build.sh
+++ travis-build.sh
@@ -10,12 +10,12 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: linux-image-mainline-lts" \
-	"Version: 0.0.4-$git_commit" \
+	"Package: linux-image-mainline-vfio" \
+	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
-	"Conflicts: linux-image-mainline-current" \
+	"Conflicts: linux-image-mainline-current, linux-image-mainline-lts" \
 	"Architecture: amd64" \
-	"Description: Meta package to install mainline Linux kernel (LTS)."
+	"Description: Meta package to install mainline Linux kernel (with VFIO as external module support)."
 
 equivs-build configuration
