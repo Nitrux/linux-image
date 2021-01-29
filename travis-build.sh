d8cdb9ba50646f524a48552bad6c05baf0a1f0d5
Uri Herrera <hello@uriherrera.com>
2021-01-29 04:29:29 -0600

    create package

--- travis-build.sh
+++ travis-build.sh
@@ -10,12 +10,12 @@ git_commit=$(git rev-parse --short HEAD)
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
-	"Package: linux-image-mainline-lts" \
-	"Version: 0.1.2-$git_commit" \
+	"Package: linux-image-liquorix" \
+	"Version: 0.0.1-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
-	"Conflicts: linux-image-mainline-current" \
+	"Conflicts: " \
 	"Architecture: amd64" \
-	"Description: Meta package to install mainline Linux kernel (LTS)."
+	"Description: Meta package to install liquorix Linux kernel."
 
 equivs-build configuration
